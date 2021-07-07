0395da482befedac6a72a04721bd299edcf9d247
Your Name <you@example.com>
2021-07-07 12:05:02 -0400

    Adjustments

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,13 @@
-const APP_PREFIX= 'BudgetTracker-';
-const VERSION= 'version_01';
-const CACHE_NAME= APP_PREFIX + VERSION;
+"use strict";
+
+console.log("Hello from your friendly neighborhood service worker!");
 
 const FILES_TO_CACHE= [
     '/',
   '/index.html',
   '/js/index.js',
   '/js/idb.js',
-//   '/manifest.json',
+  '/manifest.json',
   '/css/styles.css',
   '/icons/icon-72x72.png',
   '/icons/icon-96x96.png',
@@ -16,50 +16,72 @@ const FILES_TO_CACHE= [
   '/icons/icon-152x152.png',
   '/icons/icon-192x192.png',
   '/icons/icon-384x384.png',
-  '/icons/icon-512x512.png'  
-]
+  '/icons/icon-512x512.png',
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+  //   "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+  ]
 
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
-            console.log('installing cache:' + CACHE_NAME)
-            return cache.addAll(FILES_TO_CACHE)
-        })
-    )
-})
+const CACHE_NAME = `static-cache-v1`;
+const DATA_CACHE_NAME = `data-cache-v1`;
 
-self.addEventListener('activate', function(e) {
-    e.waitUntil(
-        caches.keys().then(function (keyList) {
-            let cacheKeeplist= keyList.filter(function (key) {
-                return key.indexOf(APP_PREFIX);
-            });
-            cacheKeeplist.push(CACHE_NAME);
+// Install
+self.addEventListener(`install`, (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log(`Your files were pre-cached successfully!`);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 
-            return Promise.all(
-                keyList.map(function(key, i) {
-                    if (cacheKeeplist.indexOf(key) === -1) {
-                        console.log('deleting cache : ' + keyList[i]);
-                        return caches.delete(keyList[i]);
-                    }
-                })
-            );
-        })
-    )
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', function(e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-        caches.match(e.request).then(function(request) {
-            if (request) {
-                console.log('responding with cache : ' + e.request.url)
-                return request
-            }
-            else {
-                console.log('file is not cached, fetching : ' + e.request.url)
-                return fetch(e.request)
-            }
+// Activate
+self.addEventListener(`activate`, (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log(`Removing old cache data`, key);
+            return caches.delete(key);
+          }
+          return undefined;
         })
+      )
     )
-})
+  );
+
+  self.clients.claim();
+});
+
+// Fetch event
+self.addEventListener(`fetch`, (evt) => {
+  if (evt.request.url.includes(`/api/`)) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) =>
+          fetch(evt.request)
+            .then((response) => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+
+              return response;
+            })
+            // Network request failed, try to get it from the cache.
+            .catch(() => cache.match(evt.request))
+        )
+        .catch((err) => console.log(err))
+    );
+  } else {
+    evt.respondWith(
+      caches
+        .match(evt.request)
+        .then((response) => response || fetch(evt.request))
+    );
+  }
+});
