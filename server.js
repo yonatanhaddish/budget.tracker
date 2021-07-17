0c6292b6cbc72273fe73d2ebfcd270650f5275b7
Your Name <you@example.com>
2021-07-17 16:59:01 -0400

    trial

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3006;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/Budget-tracker";
 
 const app = express();
